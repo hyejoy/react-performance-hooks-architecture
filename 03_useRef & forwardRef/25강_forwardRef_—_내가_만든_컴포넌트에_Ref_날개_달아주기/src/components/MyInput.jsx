3905914fde599a67083976d71472d0e909181954
hyejoy <devchoi97@gmail.com>
2026-03-22 15:25:19 +0900

    feat: forwardRef - 자식컴포넌트를 감싸 외부로 부터 ref전달을 받을 준비가 되었음을 알림

--- 03_useRef & forwardRef/25강_forwardRef_—_내가_만든_컴포넌트에_Ref_날개_달아주기/src/components/MyInput.jsx
+++ 03_useRef & forwardRef/25강_forwardRef_—_내가_만든_컴포넌트에_Ref_날개_달아주기/src/components/MyInput.jsx
@@ -1,9 +1,13 @@
 import { forwardRef } from "react";
 const MyInput = forwardRef((props, ref) => {
   return (
-    <div style={{ marginBottom: '15px' }}>
-      <label style={{ display: 'block' }}>{props.label}</label>
-      <input ref={ref} {...props} style={{ padding: '8px', border: '2px solid blue' }} />
+    <div style={{ marginBottom: "15px" }}>
+      <label style={{ display: "block" }}>{props.label}</label>
+      <input
+        ref={ref}
+        {...props}
+        style={{ padding: "8px", border: "2px solid blue" }}
+      />
     </div>
   );
 });
